190027462f1b86b838ef1c7eabffcc93a847f85b
xunjianxiang <xunjianxiang@gmail.com>
2018-05-18 12:04:20 +0800

    feat(server): app 下挂载 外部 api 配置
    g

--- server/app.js
+++ server/app.js
@@ -3,4 +3,22 @@ const meanieMongooseToJson = require('meanie-mongoose-to-json');
 // app.js
 module.exports = app => {
   app.mongoose.plugin(meanieMongooseToJson);
+  app.api = {
+    gitlab: {
+      group: {
+        list: '/groups',
+        members: '/groups/{1}/members',
+      },
+      project: {
+        list: '/groups/{1}/projects',
+        members: '/projects/{1}/members',
+      },
+      branch: {
+        list: '/projects/{1}/repository/branches',
+      },
+      tag: {
+        list: '/projects/{1}/repository/tags',
+      },
+    },
+  };
 };
